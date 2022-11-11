65f861caf9c4cfd90b96422c7bbdb84ed5dc2d73
Izzy Muerte <63051+bruxisma@users.noreply.github.com>
2022-11-11 12:54:57 -0800

    Moved to a "functional" approach for creating symlinks
    I also had forgotten to uncomment the code that did the actual
    symlinking. oops!
    
    I feel that this code is definitely a lot cleaner. I might also want to
    look into placing the powershell files into a psh folder, which is then
    symlinked to ~/Documents/WindowsPowershell rather than individual files,
    so that I don't have to worry about files being loaded incorrectly if
    they depend on each other.

--- setup.py
+++ setup.py
@@ -5,9 +5,8 @@
 
 # Errors out if it can't clean up symlinks, or directories.
 # Needs to be run as administrator or with the privelege to create symlinks on
-# windows. this script will not work on Windows XP (and really who the hell
-# is using XP in this day and age? :/)
-
+# windows. this script will not work on Windows XP, as I don't work on Windows
+# XP
 
 from glob import glob
 
@@ -18,36 +17,43 @@ dotfile_list = glob('*rc')
 ps_list = glob('*.ps1')
 dir_list = ['vim']
 
-def symlink(source, path, directory=False):
-    ''' Given a source and 'output name', will symlink the source to
-    the output name such that it is connected to the $HOME
+def symlink(iterable, func, directory=False):
+    ''' Given an iterable of filenames, and a func that takes a string, and
+    returns the expected formatted version, create symlinks to all files within
+    the iterable. (If the iterable is full of directories, set directory to
+    True)
+
+    Will cause a complete exit with an error code of 1 if the symlink fails.
+    Will cause a complete exit with an error code of 2 if the symlink already
+    exists and could not be removed.
     '''
-    link = os.path.expanduser(os.path.join('~', path))
-    print('{} -> {}'.format(source, link))
-    #try: os.symlink(source, link, target_is_directory=directory)
-    #except OSError as e:
-    #    print('Could not symlink {}'.format(e))
-    #    sys.exit(1)
+    for item in iterable:
+        link = os.path.expanduser(os.path.join('~', func(item)))
+        print('{} -> {}'.format(item, link))
+        if not directory and os.path.exists(link)
+            try: os.remove(link)
+            except OSError as e:
+                print('Could not remove old symlink {}: {}'.format(link, e))
+                sys.exit(2)
+        try: os.symlink(item, link, target_is_directory=directory)
+        except OSError as e:
+            print('Could not symlink {}: {}'.format(item, e))
+            sys.exit(1)
 
 if __name__ == '__main__':
-    # We do the dotfiles first
     # Hacky way to remove zsh stuff from being symlinked in windows
     dotfile_list = [item for item in dotfile_list
             if 'zsh' not in item and sys.platform == 'win32']
-    for dot in dotfile_list:
-        out = '{}{}'.format('.' if sys.platform != 'win32' else '_', dot)
-        symlink(dot, out)
-    # now move the directories
-    for dir in dir_list:
-        out = ('{}files' if sys.platform == 'win32' else '.{}').format(dir)
-        symlink(dir, out, directory=True)
-    # We do powershell stuff last since it is only for windows.
+    # Now we do the actual symlinking
+    symlink(dotfile_list,
+        lambda x: '{}{}'.format('.' if sys.platform != 'win32' else '_', x))
+    symlink(dir_list,
+        lambda x: ('{}files' if sys.platform != 'win32' else '.{}').format(x),
+        directory=True)
+    # Powershell stuff is last, since it is _only_ for windows
     if sys.platform == 'win32':
         temp = os.path.join('~', 'Documents', 'WindowsPowershell')
         path = os.path.expanduser(temp)
         os.makedirs(path, exist_ok=True)
-        for ps in ps_list:
-            out = os.path.join('Documents', 'WindowsPowershell', ps)
-            symlink(ps, out)
-        
-        
+        symlink(ps_list,
+            lambda x: os.path.join('Documents', 'WindowsPowershell', x))
