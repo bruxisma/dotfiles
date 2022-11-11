fcc1b171522de7902178315d5077d5b38f9ed8c3
Izzy Muerte <63051+bruxisma@users.noreply.github.com>
2022-11-11 12:54:57 -0800

    Added an "alias" section where useful aliases will be applied
    Added the 'less' alias for the 'more' cmdlet

--- psh/profile.ps1
+++ psh/profile.ps1
@@ -4,6 +4,7 @@ import-module development
 
 # Set up the powershell prompt
 # We start in ~/ at all times. ALWAYS D:<
+# This is to keep the behavior that my zshell follows on OS X
 set-location $HOME
 # At some point there will be a 'python' module which will use stuff like
 # virtualenv, and act as virtualenv wrapper tools and stuff.
@@ -12,11 +13,16 @@ set-python
 # There is nothing wrong with us calling
 # `set-msvc [some other version] [some other toolchain]`
 # as it will just overwrite it :)
+# A note however, that calling the x86 version will get more "tools" available
+# for use.
 set-msvc
 
 # both git and mercurial will use the $EDITOR value in powershell, so
 # we get the actual path and set it.
 $env:EDITOR = get-vimpath
+
+# Useful aliases to be more "unixy"
+set-alias less more
 
 # Blue is nice, but it messes with some of my programs :/
 $window = (get-host).UI.RawUI
