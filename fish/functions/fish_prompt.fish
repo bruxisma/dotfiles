03e6ac6b24182bd4fd2911620a86953046ca7967
Izzy Muerte <63051+bruxisma@users.noreply.github.com>
2022-11-11 12:57:00 -0800

    Fix fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,6 +1,6 @@
 function fish_prompt --description 'Write out the prompt'
   set -l home_escaped (echo -n $HOME | sed 's/\//\\\\\//g')
-  set -l pwd (echo -n $PWD | sed 's/^$home_escaped/~/' | sed 's/ /%20/g')
+  set -l pwd (echo -n $PWD | sed "s/^$home_escaped/~/" | sed 's/ /%20/g')
   set -l prompt_symbol ''
   switch $USER
     case root; set prompt_symbol '#'
