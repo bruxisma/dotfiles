a744130be9ba79fcb4e84f66c302be893602d58f
Izzy Muerte <63051+bruxisma@users.noreply.github.com>
2022-11-11 12:54:55 -0800

    Modified the install script to work with the new powershell script
     * Fixed up the powershell script to be a bit cleaner

--- profile.ps1
+++ profile.ps1
@@ -22,10 +22,11 @@ $env:EDITOR = 'gvim.exe'
 .NOTES
   Parts of this function are taken from stack overflow and Lee Holmes.
 #>
-function msvc([int]$version=10, [string]$type='x86') {
+function set-msvc([int]$version=10, [string]$type='x86') {
   $env_var = 'VS' + $version + '0COMNTOOLS'
   $vs_tools = [environment]::GetEnvironmentVariable($env_var, 'Machine')
-  $run_string = '"' + $vs_tools + '..\..\VC\vcvarsall.bat' + '" ' + $type + ' & set'
+  $vc_path = '..\..\VC\vcvarsall.bat'
+  $run_string = '"' + $vs_tools + $vc_path + '" ' + $type + ' & set'
   cmd.exe /c $run_string | foreach-object {
       $p, $v = $_.split('=')
       set-item -path env:$p -value $v
@@ -35,12 +36,23 @@ function msvc([int]$version=10, [string]$type='x86') {
 function prompt {
   write-host ('[' + [environment]::UserName + '@' +
               [environment]::MachineName.ToLower() + ']:' +
-              (pwd).Path.Replace($HOME, '~') + '$') -NoNewline
+              (pwd).Path.Replace('\', '/').Replace($HOME, '~') +'$') -NoNewline
   return ' '
 }
 
 # run garbage collection
 function run-gc() { [void]([System.GC]::Collect()) }
 
-#set-location $HOME
-msvc 10
+
+# Set up the powershell prompt
+# We start in ~/ at all times. ALWAYS D:<
+# We also want msvc set to VS2010 by default, with x86. There is nothing wrong
+# with us calling set-msvc [some other version] [some other toolchain]
+# as it will just overwrite it :)
+set-location $HOME
+set-msvc 10 x86
+
+# Blue is nice, but it messes with some of my programs :/
+$window = (get-host).UI.RawUI
+$window.BackgroundColor = "black"
+clear-host
