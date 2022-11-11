1ed1e18f2883baf87239fc9283f654950d3f0402
Izzy Muerte <63051+bruxisma@users.noreply.github.com>
2022-11-11 12:57:13 -0800

    🐛 Fix typo in nvim/after/ftplugin/go.lua

--- nvim/after/ftplugin/go.lua
+++ nvim/after/ftplugin/go.lua
@@ -1,6 +1,6 @@
-local golang = vim.api.nvim_create_autogroup("goformat", { clear = true })
+local golang = vim.api.nvim_create_augroup("goformat", { clear = true })
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*.go",
   group = golang,
   callback = function() require("go.format").goimport() end
-}
+})
