9842fd9fa522e094ee38b5494ae16b605a26cca8
Izzy Muerte <63051+bruxisma@users.noreply.github.com>
2022-11-11 12:57:06 -0800

    :sparkles: Can now use exa` command on windows

--- pwsh/Windows.ps1
+++ pwsh/Windows.ps1
@@ -4,10 +4,13 @@ function global:Activate-Venv {
   }
 }
 
+function global:Invoke-Exa { & bash.exe -c "`$HOME/.cargo/bin/exa $args" }
+
 $env:EDITOR = (Get-ItemProperty HKLM:\SOFTWARE\Vim\Gvim\).Path
 
 Set-Alias clear Clear-Terminal -Scope Global
 Set-Alias open Start-Process -Scope Global
+Set-Alias exa Invoke-Exa -Scope Global
 
 Append-Path 'C:\MinGW\bin'
 Append-Path $env:EDITOR
