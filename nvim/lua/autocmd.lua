e5b08a9ccfe44bbb83c0af49e817a2400718fb5c
Izzy Muerte <63051+bruxisma@users.noreply.github.com>
2024-08-26 12:57:36 -0700

    ✨ Add an easier way to customize the filetype for buffers
    ✨ Add `.wxs` and `.tofu`
    
    Signed-off-by: Izzy Muerte <63051+bruxisma@users.noreply.github.com>

--- nvim/lua/autocmd.lua
+++ nvim/lua/autocmd.lua
@@ -1,22 +1,16 @@
-vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-  pattern = {
-    "*/.local/share/ssh/*.conf",
-    "*/.config/ssh/*.conf",
-  },
-  command = "setfiletype sshconfig",
-})
+local function filetype(filetype, pattern)
+  vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+    pattern = pattern,
+    command = string.format("setfiletype %s", filetype)
+  })
+end
 
-vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-  pattern = {
-    vim.fs.normalize(vim.fs.joinpath(vim.env.VIMRUNTIME, "doc", "*.txt")),
-    vim.fs.normalize(vim.fs.joinpath(vim.fn.stdpath("data"), "lazy", "*", "doc", "*.txt")),
-  },
-  command = "setfiletype help",
-})
-
-vim.api.nvim_create_autocmd({ "BufNewFile", "Bufread" }, {
-  pattern = { "*.tofu" },
-  command = "setfiletype hcl"
+filetype("sshconfig", { "*/.local/share/ssh/*.conf", "*/.config/ssh/*.conf" })
+filetype("hcl", { "*.tf", "*.tofu" })
+filetype("xml", { "*.wxs" })
+filetype("help", {
+  vim.fs.normalize(vim.fs.joinpath(vim.env.VIMRUNTIME, "doc", "*.txt")),
+  vim.fs.normalize(vim.fs.joinpath(vim.fn.stdpath("data"), "lazy", "*", "doc", "*.txt")),
 })
 
 -- I'm unsure if I need these anymore
