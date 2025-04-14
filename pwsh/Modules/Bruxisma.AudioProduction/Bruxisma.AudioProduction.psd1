cd705c1365c7b536eae0271fef1c6c9db85bc3a6
Izzy Muerte <63051+bruxisma@users.noreply.github.com>
2025-04-14 10:22:06 -0700

    ✨ Move personal script into module
    Signed-off-by: Izzy Muerte <63051+bruxisma@users.noreply.github.com>

---
+++ pwsh/Modules/Bruxisma.AudioProduction/Bruxisma.AudioProduction.psd1
@@ -0,0 +1,15 @@
+@{
+  ModuleVersion = '1.0.0'
+  GUID = '8643b580-72e5-4f52-bb4d-0f0acaf3e2b5'
+  Author = 'Bruxisma'
+  Copyright = '© Izzy Muerte.'
+  Description = 'Common Commands for Managing My Setup'
+  PowerShellVersion = '7.5'
+  CompatiblePSEditions = @('Core')
+  RootModule = "Bruxisma.AudioProduction"
+  FunctionsToExport = @('Install-SerumPack')
+  CmdletsToExport = @()
+  VariablesToExport = '*'
+  AliasesToExport = @()
+  PrivateData = @{}
+}
---
+++ pwsh/Modules/Bruxisma.AudioProduction/Bruxisma.AudioProduction.psm1
@@ -0,0 +1,42 @@
+# TODO: Support file
+function Install-SerumPack {
+  [CmdletBinding(SupportsShouldProcess)]
+  param(
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string]$Path,
+    [String]$Name,
+    [bool]$StripRedundant = $true,
+    [bool]$Symlink = $true
+  )
+
+  begin {
+    $XferRecords = $IsWindows `
+      ? (Join-Path ([Environment]::GetFolderPath("MyDocuments")) "Xfer") `
+      : "/Library/Audio/Presets/Xfer Records"
+    $Presets = Join-Path $XferRecords "Serum Presets" "Presets"
+    $Tables = Join-Path $XferRecords "Serum Presets" "Tables"
+    $Noises = Join-Path $XferRecords "Serum Presets" "Noises"
+  }
+
+  process {
+    if (!(Test-Path -LiteralPath $Path -PathType Container)) {
+      Write-Error -Message "'${Path}' is not a directory or does not exist" -Category InvalidArgument
+      return
+    }
+
+    if (!($PSBoundParameters.ContainsKey("Name"))) {
+      $Name = (Get-Item $Path).BaseName
+    }
+
+    if ($StripRedundant) {
+      $Name = $Name.Replace("Presets", "").Replace("Preset", "").Replace("Serum", "")
+    }
+
+    $Destination = Join-Path $Presets $Name
+
+    if ($PSCmdlet.ShouldProcess($Path, "Link ${Name} to ${Path}")) {
+      New-Item -Path $Destination -Value $Path -Type SymbolicLink
+    }
+  }
+}
