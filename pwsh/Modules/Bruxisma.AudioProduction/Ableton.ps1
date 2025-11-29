b21c80779c783be808298740c451509f42b78973
Izzy Muerte <63051+bruxisma@users.noreply.github.com>
2025-11-29 04:14:19 -0800

    🐛 Fix finding the ableton user library on macOS
    Signed-off-by: Izzy Muerte <63051+bruxisma@users.noreply.github.com>

--- pwsh/Modules/Bruxisma.AudioProduction/Ableton.ps1
+++ pwsh/Modules/Bruxisma.AudioProduction/Ableton.ps1
@@ -2,14 +2,15 @@ function Find-AbletonUserLibrary {
   [CmdletBinding()]
   param([Version]$Version)
 
-  # TODO: Support macOS
-  $Live = Get-ChildItem -LiteralPath (Join-Path $env:APPDATA Ableton) -Directory -Exclude "*Reports"
+  $Preferences = $IsWindows ? @("Preferences") : @()
+  $Paths = $IsWindows ? @($env:APPDATA) : @($HOME, "Library", "Preferences")
+
+  $Live = Get-ChildItem -LiteralPath (Join-Path @Paths Ableton) -Directory -Exclude "*Reports"
         | Sort-Object -Property Name
         | Select-Object -Last 1
 
-  # TODO: Handle macOS
   # TODO: Handle `$Version`
-  $Library = [XML](Get-Content (Join-Path $Live Preferences Library.cfg))
+  $Library = [XML](Get-Content (Join-Path $Live @Preferences Library.cfg))
   $Path = $Library.Ableton.ContentLibrary.UserLibrary.LibraryProject.ProjectPath.Value
   if ($Path -eq $null) { return $Path }
   Get-Item (Join-Path $Path "User Library")
